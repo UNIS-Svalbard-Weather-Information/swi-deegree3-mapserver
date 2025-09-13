ecbbb76e2f712e22fd91798f03ec3b1462f46043
Louis Pauchet <louis.pauchet@insa-rouen.fr>
2025-09-13 21:44:30 +0200

    update setup script

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ This Docker container automates the setup and execution of a [deegree](https://w
 |-----------------------------|--------------------------------------------------|----------|----------------------------------------------------|
 | `SWI_DEEGREE_ADMIN_PWD`     | Admin password for the deegree console.          | No       | None                                               |
 | `SWI_DEEGREE_CONFIG_REPO`   | Git repository URL for the deegree configuration. | No       | `https://github.com/UNIS-Svalbard-Weather-Information/swi-deegree3-configuration.git` |
+| `SWI_DEEGREE_REST_API_KEY` | API Key to communicate to use the RESTApi | No | None |
 
 ---
 
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Define the target directory
 TARGET_DIR="/root/.deegree"
+DATA_DIR="/root/data"
 
 # Set default repository if SWI_DEEGREE_CONFIG_REPO is not defined
 REPO_URL=${SWI_DEEGREE_CONFIG_REPO:-"https://github.com/UNIS-Svalbard-Weather-Information/swi-deegree3-configuration.git"}
@@ -15,10 +16,29 @@ if [ -z "$(ls -A "$TARGET_DIR")" ]; then
     # Check if SWI_DEEGREE_ADMIN_PWD environment variable exists
     if [ -n "$SWI_DEEGREE_ADMIN_PWD" ]; then
         echo "SWI_DEEGREE_ADMIN_PWD is set. Creating console.pw file..."
-        echo "$SWI_DEEGREE_ADMIN_PWD" > "$TARGET_DIR/swi/console.pw"
+        echo "$SWI_DEEGREE_ADMIN_PWD" > "$TARGET_DIR/console.pw"
     else
         echo "SWI_DEEGREE_ADMIN_PWD is not set. Skipping console.pw creation."
     fi
+
+    # Check if SWI_DEEGREE_ADMIN_PWD environment variable exists
+    if [ -n "$SWI_DEEGREE_REST_API_KEY" ]; then
+        echo "SWI_DEEGREE_REST_API_KEY is set. Creating console.pw file..."
+        echo "$SWI_DEEGREE_REST_API_KEY" > "$TARGET_DIR/config.apikey"
+    else
+        echo "SWI_DEEGREE_REST_API_KEY is not set. Skipping console.pw creation."
+    fi
+
+    # Ensure target directories exist
+    mkdir -p "$DATA_DIR/themes/"
+
+    # Move files
+    mv "$TARGET_DIR"/swi/themes/* "$DATA_DIR/themes/"
+
+    # Create symbolic links
+    for file in "$DATA_DIR"/themes/*; do
+        ln -s "$file" "$TARGET_DIR/swi/themes/"
+    done
 
     # Create webapps.properties file
     echo "Creating webapps.properties file..."
